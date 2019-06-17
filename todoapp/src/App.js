2dd1fe90e13d54e3a0030d1470f1c476582d0087
Andrew Dillon <majik895@gmail.com>
2019-06-17 18:49:56 -0400

    Nice todo app not Redux!!!

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 //=====Components=====//
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
  class App extends Component {
    state = {
@@ -10,15 +11,34 @@ import Todos from './Todos'
        {id: 2, content: 'Play Mario Cart.'}
      ]
    }
+   deleteTodo = (id) => {
+     console.log(id);
+     const todos = this.state.todos.filter(todo => {
+       return todo.id !==id
+     });
+     this.setState({
+       todos
+     })
+   }
+   addTodo = (todo) => {
+      todo.id = Math.random();
+      let todos =[...this.state.todos, todo];
+      this.setState({
+        todos: todos
+      })
+   }
+
+
   render() {
     return (
       <div>
         <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
     </div>
       </div>
-    )
+    );
   }
 }
 
